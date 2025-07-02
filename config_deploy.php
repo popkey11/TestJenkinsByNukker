35722aa50b22bbc1fcadd5ff67eb63e00fdef379
Wuttichai <CWuttichai@ada-soft.com>
2025-07-02 17:29:30 +0700

    test pust

---
+++ config_deploy.php
@@ -0,0 +1,3 @@
+<?php
+echo "This is a configuration file for deployment. It contains settings and paths for the application.\n";
+echo "Please ensure that the paths and configurations are correct before deploying.\n";
